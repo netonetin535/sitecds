f3fb2a384a17a4580453d6e834791490d5bf51ad
netonetin535 <j70051842@gmail.com>
2025-07-03 15:16:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,28 @@ fetch('cds.json')
   .then(res => res.json())
   .then(cds => {
     const container = document.getElementById('cd-container');
+
     cds.forEach(cd => {
       const card = document.createElement('div');
-      card.className = 'cd-card';
+      card.classList.add('cd-card');
+
       card.innerHTML = `
-        <img class="cd-cover" src="${cd.capa}" alt="${cd.titulo}">
+        <img src="${cd.capa}" alt="${cd.titulo}" class="cd-cover">
         <div class="cd-info">
-          <div class="cd-title">${cd.titulo}</div>
-          <a class="download-button" href="${cd.arquivo}" download>
-            <i data-lucide="download"></i> Baixar CD
+          <h3 class="cd-title">${cd.titulo}</h3>
+          <a href="${cd.arquivo}" class="download-button" download>
+            <i data-lucide="download"></i>
+            Baixar CD
           </a>
         </div>
       `;
+
       container.appendChild(card);
     });
+
+    // Ativa os ícones Lucide
+    lucide.createIcons();
+  })
+  .catch(error => {
+    console.error("Erro ao carregar CDs:", error);
   });
